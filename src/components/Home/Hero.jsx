f63493cfa8135d9594978708f7b13d5f46a8913e
Hanson Tram <htram.dev@gmail.com>
2023-05-01 17:00:15 -0700

    Text on hero is now displaying on img

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,19 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./Hero.css";
-import heroImg from '../../assets/Home/ts-wmns.jpeg'
+import heroImg from "../../assets/Home/ts-wmns.jpeg";
 
 const Hero = () => {
   return (
     <section className="hero-container">
-        <div className="img-wrapper">
-        <img className="hero-img" src={heroImg}/>
+      <div>
+        <img className="hero-img" src={heroImg} />
+      </div>
+      <div className="hero-text">
         <h2>TRAVIS SCOTT X WMNS AIR JORDAN 1 RETRO LOW OG 'OLIVE'</h2>
         <Link className="link" to="/">
-            Shop Now
-          </Link>
-        </div>
+          Shop Now
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
